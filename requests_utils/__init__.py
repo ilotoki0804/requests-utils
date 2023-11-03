37fcda871d932b55801a1192462f07adae203063
ilotoki0804 <ilotoki0804@gmail.com>
2023-11-03 23:20:17 +0900

    문서 추가 및 PEP 8 준수

--- requests_utils/__init__.py
+++ requests_utils/__init__.py
@@ -1,7 +1,13 @@
-from . import requests_ as requests
-from .custom_defaults import CustomDefaults
-from .header_utils import clean_headers
-from .sessions_with_tools import Session
+"""
+██████╗░███████╗░██████╗░██╗░░░██╗███████╗░██████╗████████╗░██████╗░░░░░░██╗░░░██╗████████╗██╗██╗░░░░░░██████╗
+██╔══██╗██╔════╝██╔═══██╗██║░░░██║██╔════╝██╔════╝╚══██╔══╝██╔════╝░░░░░░██║░░░██║╚══██╔══╝██║██║░░░░░██╔════╝
+██████╔╝█████╗░░██║██╗██║██║░░░██║█████╗░░╚█████╗░░░░██║░░░╚█████╗░█████╗██║░░░██║░░░██║░░░██║██║░░░░░╚█████╗░
+██╔══██╗██╔══╝░░╚██████╔╝██║░░░██║██╔══╝░░░╚═══██╗░░░██║░░░░╚═══██╗╚════╝██║░░░██║░░░██║░░░██║██║░░░░░░╚═══██╗
+██║░░██║███████╗░╚═██╔═╝░╚██████╔╝███████╗██████╔╝░░░██║░░░██████╔╝░░░░░░╚██████╔╝░░░██║░░░██║███████╗██████╔╝
+╚═╝░░╚═╝╚══════╝░░░╚═╝░░░░╚═════╝░╚══════╝╚═════╝░░░░╚═╝░░░╚═════╝░░░░░░░░╚═════╝░░░░╚═╝░░░╚═╝╚══════╝╚═════╝░
+
+Various convenient features about requests.
+"""
 
 __title__ = "requests_utils"
 __description__ = 'Various convenient features about requests.'
@@ -12,3 +18,9 @@ __version__ = str.join('.', map(str, __version_info__))
 __author__ = "ilotoki0804"
 __author_email__ = "ilotoki0804@gmail.com"
 __license__ = "MIT License"
+
+from . import requests_ as requests
+from .custom_defaults import CustomDefaults
+from .header_utils import clean_headers
+from .sessions_with_tools import Session
+from .souptoolsclass import SoupTools

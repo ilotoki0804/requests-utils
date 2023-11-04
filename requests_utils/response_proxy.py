e29fa038524d932319e8bda39b1307b329d1b2ff
ilotoki0804 <ilotoki0804@gmail.com>
2023-11-04 16:38:17 +0900

    버그 수정

--- requests_utils/response_proxy.py
+++ requests_utils/response_proxy.py
@@ -9,3 +9,4 @@ class ResponseProxy(Response, SoupTools):
     def __init__(self, response):
         state = response.__reduce__()[2]
         self.__setstate__(state)  # type: ignore
+        self.response = response

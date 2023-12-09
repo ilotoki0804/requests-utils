d378f93e548b8f2d76fd4101a2197d6085f92ef8
ilotoki0804 <ilotoki0804@gmail.com>
2023-12-09 20:21:38 +0900

    (0.5.0) 빌드 환경 변화

--- build.py
+++ build.py
@@ -9,10 +9,6 @@ try:
     shutil.rmtree('build')
 except FileNotFoundError:
     os.mkdir('dist')
-os.system(r'.\.venv312\Scripts\python.exe setup.py sdist bdist_wheel')
-whl_file_name = os.listdir('dist')[0]
-os.system(rf'.\.venv312\Scripts\python.exe -m pip install --force-reinstall dist/{whl_file_name}')  # --user를 추가하면 오류가 덜 날 수도 있음
-if input('Submit changes? (y or not)') in ('y', 'Y', 'ㅛ'):
-    token = Path('token.txt').read_text(encoding='utf-8')
-    subprocess.run(["twine", "upload", "-u", '__token__', "-p", token, "dist/*"], check=False)
-    os.system(r'.\.venv312\Scripts\python.exe -m pip show resoup')
+
+os.system('poetry build')
+os.system(f'poetry publish -u __token__ -p {Path("_token.txt").read_text("utf-8")}')
